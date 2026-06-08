ecf57875439c0ca03d88208aea2e685cfc1fd760
ParrotXray <b05817691@gmail.com>
2026-06-08 11:10:42 +0800

    feat: Adjust background opacity and foreground color for selected wallpaper in ExplorerItem

--- Open Wallpaper Engine/ContentView/Components/ExplorerItem.swift
+++ Open Wallpaper Engine/ContentView/Components/ExplorerItem.swift
@@ -35,10 +35,10 @@ struct ExplorerItem: SubviewOfContentView {
                 .lineLimit(2)
                 .frame(maxWidth: .infinity, minHeight: 30)
                 .padding(4)
-                .background(Color(white: 0, opacity: viewModel.imageScaleIndex == index ? 0.4 : 0.2))
+                .background(Color(white: 0, opacity: viewModel.imageScaleIndex == index ? 0.6 : 0.45))
                 .font(.footnote)
                 .multilineTextAlignment(.center)
-                .foregroundStyle(Color(white: viewModel.imageScaleIndex == index ? 0.9 : 0.7))
+                .foregroundStyle(Color.white)
             
 //            Spacer()
 //                .onHover { onHover in
